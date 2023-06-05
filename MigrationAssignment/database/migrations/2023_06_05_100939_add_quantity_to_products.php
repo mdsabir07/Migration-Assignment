fd52b86efaed8f5baa56eb6acbafcbd236ae94ee
Sabirul Islam <45106497+mdsabir07@users.noreply.github.com>
2023-06-05 16:36:09 +0600

    commit

--- MigrationAssignment/database/migrations/2023_06_05_100939_add_quantity_to_products.php
+++ MigrationAssignment/database/migrations/2023_06_05_100939_add_quantity_to_products.php
@@ -11,11 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::table('products', function (Blueprint $table) {
+            $table->integer('quantity')->nullable();
         });
+        // https://prnt.sc/BHXrpFx0g0pu
     }
 
     /**
@@ -23,6 +22,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
     }
 };
